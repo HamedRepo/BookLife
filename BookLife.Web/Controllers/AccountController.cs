af34cb16456f6b0ff9e87f5f95004f9f6adea16e
hamed <h.fakouri@yahoo.com>
2016-07-26 17:49:03 +0430

    First GitHub

--- BookLife.Web/Controllers/AccountController.cs
+++ BookLife.Web/Controllers/AccountController.cs
@@ -15,6 +15,8 @@ namespace BookLife.Web.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+
+
         private ApplicationSignInManager _signInManager;
 
         private ApplicationUserManager _userManager;
