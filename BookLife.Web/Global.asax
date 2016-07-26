cdb894a4d257d4d393674259f5c6bb0df63d38a9
hamed <h.fakouri@yahoo.com>
2016-07-26 17:20:19 +0430

    aa

---
+++ BookLife.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookLife.Web.MvcApplication" Language="C#" %>
---
+++ BookLife.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookLife.Web.Startup))]
+namespace BookLife.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
